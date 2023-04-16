505e359f80d2621b2532b49af3242569c488bdae
luciacantos <luciacantos22@gmail.com>
2023-04-16 12:25:09 +0200

    añadir nota final a los diccionarios

--- parte2.py
+++ parte2.py
@@ -1,4 +1,11 @@
-from parte1 import *
+from parte1 import calificaciones
 
 def nota_final(lista_diccionarios):
+    for diccionario in lista_diccionarios:
+        parcial1 = float(diccionario["Parcial1"]) * 0.3
+        parcial2 = float(diccionario["Parcial2"]) * 0.3
+        practicas = float(diccionario["Practicas"]) * 0.4
+        diccionario["Nota final"] = parcial1 + parcial2 + practicas
+    return lista_diccionarios
+
 
